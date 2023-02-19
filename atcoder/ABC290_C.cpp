e3fb81145caa2ad20660ae327d59634cc471c0cc
ackerman-levi-0007 <gyash1907@gmail.com>
2023-02-19 14:49:37 +0530

    atcoder_290

---
+++ atcoder/ABC290_C.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long ;
+using pii = pair<int , int>;
+using pll = pair<ll, ll>;
+
+
+//pairs
+#define ss second
+#define ff first
+
+
+// vectors
+#define sz(x) (int)(x).size()
+#define all(x) begin(x), end(x)
+#define rall(x) (x).rbegin(), (x).rend()
+#define sor(x) sort(all(x))
+#define rsz resize
+#define ins insert
+#define ft front()
+#define bk back()
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+
+//arrays
+#define mem(x , y) memset(x , y , sizeof(x) )
+
+const int N = 1e5 + 10 , mod =  1000000007;
+
+//helper funcs
+ll cdiv(ll a, ll b) { return a / b + ((a ^ b) > 0 && a % b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a / b - ((a ^ b) < 0 && a % b); } // divide a by b rounded down
+
+
+void solve() {
+
+    ll n, k;
+    cin >> n >> k;
+    ll sol[k+1] = {0};
+    for(ll i=0 ; i<n ; i++){
+    	ll x;
+    	cin >> x;
+    	if(x<=k) sol[x]++;
+    }
+    ll ans=0;
+    for(; ans<k ; ans++){
+    	if(sol[ans] == 0) break;
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    // time_t start , end;
+
+    // time(&start);
+
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+
+
+    // time(&end);
+
+    // printf("time: %f", (double)end - start );
+
+    return 0;
+}
+
+
